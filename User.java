9561b53086569beefda15bb18637e15cecf50db1
zhanxinyuii <31892821+zhanxinyuii@users.noreply.github.com>
2017-09-13 15:31:20 +0800

    Update User.java
    第一次修改代码

--- User.java
+++ User.java
@@ -1,9 +1,7 @@
 public class user
 {
-  public void say()
-   {
-    System.out.println("I am a user!!");
-   }
-  
-  
+ public static void tell()
+ {
+   System.out.println("第一次修改github里的代码，想想还有点小激动呢！！");
+ }
 }
